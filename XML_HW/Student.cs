325a62b71b4d1cb9ec19c406e108e5afd8753516
Asselya Moldasheva <a.moldasheva@gmail.com>
2019-09-16 20:40:02 +0600

    Добавьте файлы проекта.

---
+++ XML_HW/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XML_HW
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public long MobileNumber { get; set; }
+        public string Course { get; set; }
+        public DateTime StartDate { get; set; }
+    }
+}
